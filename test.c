fae40c15bb989a9dc59f909481eda86490ad920b
mbraut576 <47878246+mbraut576@users.noreply.github.com>
2019-02-22 15:49:34 +0530

    Update test.c

--- test.c
+++ test.c
@@ -3,18 +3,16 @@
 int main()
 {
 int a=25,b=10;
-int c,k=0,*ptr;
+int c,ptr;
 int arr[3] = {5,9,4};
 c=a+b;
-read = k++*k++*k++;
-printf("Read Value = %d",k);
 ptr=fetch();
 printf("Addition = %d",c);
-printf("Ponter Value = %d",*ptr);
+printf("Ponter Value = %d",ptr);
 printf("Array Value = %d",arr[1]);
 }
 
-int* fetch()
+int fetch()
 {
 int val;
 val = 100;
